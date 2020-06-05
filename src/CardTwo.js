3005349e22398e45f785848132af4f941b53727f
Peter Cirocco <peter.cirocco@sas.com>
2020-06-05 12:06:30 -0400

    temp fix

--- src/CardTwo.js
+++ src/CardTwo.js
@@ -12,7 +12,7 @@ const CardTwo = ({id, name, description, report_url, screen_shot}) => {
         <Card  className='tc ma2 grow bw2 shadow-5' style={{width: '18rem', height: '26rem', backgroundColor: '#cdecff', textAlign:"center", display: "inline-block", padding:8}}>
             <h3>{name}</h3>
                 <Card.Body>
-                  <Card.Img variant="top" src={screen_shot} style={{width:250, height:230}}/>
+                <Card.Img variant="top" src={screen_shot} style={{width:250, height:230}}/>
                   <Card.Text>{description}</Card.Text>
                   <Button style={{backgroundColor: '#357edd'}} href = {report_url} block>View Report</Button>
                 </Card.Body>
--- src/reports.js
+++ src/reports.js
@@ -5,7 +5,7 @@ export const reports = [
         description: 'Based on data pulled from Orion',
         report_url: 'https://ccbu-viya35.aws.sas.com/links/resources/report?uri=%2Freports%2Freports%2F2dd230ca-ee5e-4998-b682-ac9cff3aa656&page=vi102866',
         button_desc: 'View Opps Report',
-        screen_shot: 'Orion_Report.SC.png'
+        screen_shot: 'SAS-logo.png'
     },
 
     {
@@ -14,6 +14,6 @@ export const reports = [
         description: 'Based on data pulled from MIDAS',
         report_url: 'https://ccbu-viya35.aws.sas.com/links/resources/report?uri=%2Freports%2Freports%2F4f69e689-823a-4e37-9470-bcbca98d94da&page=vi2777',
         button_desc: 'View MIDAS Report',
-        screen_shot: 'MIDAS_Report_SC.png'
+        screen_shot: 'SAS-logo.png'
     }
 ];
